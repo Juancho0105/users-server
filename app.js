a3390e0bc3f3d9188be1ddf1ba70a5a443a4e1f2
Juan Reyes <juana.reyesc@uqvirtual.edu.co>
2026-01-17 15:33:53 -0500

    comment the lines that using cors

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const path = require('path');
-const cors = require('cors');
+// const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
 
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, 'public')));
